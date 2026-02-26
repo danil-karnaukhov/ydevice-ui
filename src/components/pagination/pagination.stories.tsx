3ac0dec57204a196820da0595f5ed0b44e843b46
Danil Karnaukhov <karnaukhov2025@gmail.com>
2026-02-26 20:36:35 +0700

    feat(Pagination): update according to new design

--- src/components/pagination/pagination.stories.tsx
+++ src/components/pagination/pagination.stories.tsx
@@ -7,6 +7,7 @@ const meta = {
   title: 'Navigation/Pagination',
   component: Pagination,
   args: {
+    size: 'm',
     pageCount: 10,
   },
 } satisfies Meta<typeof Pagination>
@@ -19,6 +20,28 @@ export const Default: StoryFn<typeof Pagination> = (args) => {
   }
 
   return <Pagination {...args} forcePage={page - 1} onPageChange={handlePageClick} />
+}
+
+export const Sizes: StoryFn<typeof Pagination> = (args) => {
+  const [page, setPage] = useState(1)
+
+  const handlePageClick = (selectedItem: { selected: number }) => {
+    setPage(selectedItem.selected + 1)
+  }
+
+  return (
+    <div className='sb-column'>
+      <Pagination {...args} size='s' forcePage={page - 1} onPageChange={handlePageClick} />
+      <Pagination {...args} size='m' forcePage={page - 1} onPageChange={handlePageClick} />
+      <Pagination {...args} size='l' forcePage={page - 1} onPageChange={handlePageClick} />
+    </div>
+  )
+}
+
+Sizes.argTypes = {
+  size: {
+    table: { disable: true },
+  },
 }
 
 export default meta
--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -7,28 +7,32 @@ import { block } from '../../utils/bem'
 
 import './pagination.scss'
 
+export type PaginationSize = 's' | 'm' | 'l'
+
+export type PaginationProps = ReactPaginateProps & {
+  size?: PaginationSize
+}
+
 const b = block('pagination')
 
-export const Pagination = (props: ReactPaginateProps) => {
-  const { pageCount, className, ...rest } = props
+export const Pagination = (props: PaginationProps) => {
+  const { pageCount, size = 'm', className, ...rest } = props
 
   if (pageCount <= 1) return null
 
   return (
-    <div className={b(null, className)}>
-      <ReactPaginate
-        containerClassName={b('container')}
-        pageClassName={b('page')}
-        nextClassName={b('next')}
-        previousClassName={b('previous')}
-        breakClassName={b('break')}
-        activeClassName='active'
-        disabledClassName='disabled'
-        nextLabel={<ChevronRightIcon />}
-        previousLabel={<ChevronLeftIcon />}
-        pageCount={pageCount}
-        {...rest}
-      />
-    </div>
+    <ReactPaginate
+      containerClassName={b({ size }, className)}
+      pageClassName={b('page')}
+      nextClassName={b('next')}
+      previousClassName={b('previous')}
+      breakClassName={b('break')}
+      activeClassName='active'
+      disabledClassName='disabled'
+      nextLabel={<ChevronRightIcon />}
+      previousLabel={<ChevronLeftIcon />}
+      pageCount={pageCount}
+      {...rest}
+    />
   )
 }
